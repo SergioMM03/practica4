61eacfa832f7d40f86288cb0452218557e0256af
SergioMM03 <93463810+SergioMM03@users.noreply.github.com>
2026-02-24 10:57:30 -0600

    Rehace App.jsx con estructura limpia y condicionales claras

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useContext } from 'react'
+import { BudgetForm } from './components/BudgetForm'
+import { BudgetTracker } from './components/BudgetTracker'
+import ExpenseModal from './components/ExpenseModal'
+import { BudgetStateContext } from './context/BudgetContexts'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { budget } = useContext(BudgetStateContext)
+  const isValidBudget = Number(budget) > 0
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="min-h-screen">
+      <header className="bg-blue-600 py-8 max-h-72">
+        <h1 className="uppercase text-center font-black text-4xl text-white">
+          Planificador de gastos
+        </h1>
+      </header>
+
+      <main className="max-w-3xl mx-auto py-10 px-4">
+        <section className="bg-white shadow-lg rounded-lg p-10">
+          {isValidBudget ? <BudgetTracker /> : <BudgetForm />}
+        </section>
+
+        {isValidBudget && (
+          <section aria-label="acciones-de-gasto" className="relative">
+            <ExpenseModal />
+          </section>
+        )}
+      </main>
+    </div>
   )
 }
 
